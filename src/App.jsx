defe10a75f61582641a060d5943a98d9603a4f66
AyobamiH <heryourbarme@live.com>
2024-09-11 21:21:29 +0100

    file changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,31 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-import Header from "./components/DmHedgesHeader/Header.jsx";
-import Footer from "./components/DmHedgesFooter/Footer.jsx";
-import HomePage from './components/DmHedgesHomePage/HomePage.jsx';
-import DmHedgesAboutUsPage from './components/AboutUsPage/AboutUsPage.jsx';
-import ContactPage from './components/ContactPage/ContactPage.jsx';
-import FaQPage from './components/DmHedgesFaQ/FAQPage.jsx';
-import ServicesPage from './components/DmHedgesServicesPage/ServicesPage.jsx';
-import TestimonialsPage from './components/DmHedgesTestimonials/TestimonialsPage.jsx';
-import Policy from './components/DmHedgesLegal/PrivacyPolicy.jsx';
-import TermsOfService from './components/DmHedgesLegal/TermsOfService.jsx'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+import Header from './components/uizard/Header';
+import Footer from './components/uizard/Footer';
+import LandingPage from './components/uizard/LandingPage';
+import ServicesPage from './components/uizard/ServicesPage';
+import Contact from './components/uizard/Contact';
 function App() {
   return (
     <Router>
+
+      
+      {/* Header should be shown on all pages */}
       <Header />
+      
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<DmHedgesAboutUsPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="/faq" element={<FaQPage />} />
+        {/* Route for the Landing/Home page */}
+        <Route path="/" element={<LandingPage />} />
+
+        {/* Route for the Services page */}
         <Route path="/services" element={<ServicesPage />} />
-        <Route path="/testimonials" element={<TestimonialsPage />} />
-        <Route path="/privacy-policy" element={<Policy />} />
-        <Route path="/terms-of-service" element={<TermsOfService />} />
+
+        {/* You can add more routes for other pages if needed */}
+        <Route path='/contact' element={<Contact />} />
       </Routes>
+      
+      {/* Footer should be shown on all pages */}
       <Footer />
     </Router>
   );
